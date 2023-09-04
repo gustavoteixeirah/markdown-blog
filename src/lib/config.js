1aca826cbbfdb6835e751d0ff5b3c2a5cfb8f64b
Gustavo Teixeira <contact@gustavoteixeira.dev>
2023-09-03 23:46:37 -0300

    Add header component and npm configurations
    This commit adds a new svelte component `header.svelte` for controlling the navigation. Multiple links for navigation are added in this header. It also includes a toggle button for changing the theme.
    
    The commit introduces new npm configuration files `.npmrc` and `pnpm-lock.yaml`.
    The `.npmrc` file is set to enforce engine compatibility and ensure package resolutions are of the highest version.
    The introduced `pnpm-lock.yaml` provides details about the versions and integrity of all the project's dependencies.
    
    Introduced configurations ensure consistent development environments across different devices and setups for the entire development team.

---
+++ src/lib/config.js
@@ -0,0 +1,7 @@
+// place files you want to import through the `$lib` alias in this folder.
+
+import { dev } from '$app/environment'
+
+export const title = 'Shakespear'
+export const description = 'Sveltekit blog for posts'
+export const url = dev ? 'http://localhost:5173/' : 'url'
